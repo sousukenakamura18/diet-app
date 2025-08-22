23c8931a6bd246878e4923dc10389092c068b30c
sousukenakamura18 <sshi98927@gmail.com>
2025-08-22 15:38:29 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -9,13 +9,28 @@ const urlsToCache = [
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
   self.skipWaiting();
 });
 
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(resp => resp || fetch(event.request))
+    caches.match(event.request)
+      .then(resp => resp || fetch(event.request))
+      .catch(() => caches.match('./index.html')) // オフライン対応
   );
 });
